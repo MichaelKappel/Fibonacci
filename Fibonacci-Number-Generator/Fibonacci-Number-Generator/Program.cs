07082d24870c93f0badac527faf5046be88b8267
Michael Kappel <mike@ns12.com>
2019-02-13 17:04:27 -0600

    Update math program

--- Fibonacci-Number-Generator/Fibonacci-Number-Generator/Program.cs
+++ Fibonacci-Number-Generator/Fibonacci-Number-Generator/Program.cs
@@ -21,32 +21,22 @@ namespace Fibonacci_Number_Generator
             if (command == "1")
             {
                 UInt32 fibonaccisCount = 2758065;
-                Number fibonacci1 = env.OpenNumberFile(@"D:\git\Fibonacci\Fibonacci-Number-Generator\Fibonacci\6\2758064.txt");
-                Number fibonacci2 = env.OpenNumberFile(@"D:\git\Fibonacci\Fibonacci-Number-Generator\Fibonacci\6\2758065.txt");
+                Number fibonacci1 = env.OpenNumberFile(@"D:\git\Fibonacci\Fibonacci-Number-Generator\Fibonacci\6\", @"2758064.txt");
+                Number fibonacci2 = env.OpenNumberFile(@"D:\git\Fibonacci\Fibonacci-Number-Generator\Fibonacci\6\", @"2758065.txt");
                 
                 Boolean stop = false;
-                var lastFibonaccisFolder = String.Empty;
                 while (!stop)
                 {
                     startTime = DateTime.Now;
                     Number fibonacciNext = fibonacci1 + fibonacci2;
                     String fibonacci = fibonacciNext.GetCharArray();
                     fibonaccisCount += 1;
-                    var currentFibonaccisFolder = String.Format("../../../../Fibonacci/{0}/", fibonaccisCount.ToString().Length);
-                    if (lastFibonaccisFolder != currentFibonaccisFolder)
-                    {
-                        if (!Directory.Exists(currentFibonaccisFolder))
-                        {
-                            Directory.CreateDirectory(currentFibonaccisFolder);
-                        }
-                        lastFibonaccisFolder = currentFibonaccisFolder;
-                    }
-                    fibonacciNext.SaveFile(String.Format("{0}{1}.txt", currentFibonaccisFolder, fibonaccisCount.ToString()));
+          
+                    fibonacciNext.SaveFile(fibonacciNext.Size.ToString(), fibonaccisCount.ToString());
 
                     fibonacci1 = fibonacci2;
                     fibonacci2 = fibonacciNext;
                     Console.WriteLine("{0} length {1}", fibonaccisCount, fibonacciNext.Size);
-
                 }
             }
             else
